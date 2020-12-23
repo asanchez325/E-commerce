29115cb8a410fce41991d35e1a22f01df7bfd1f2
Imperial Design <amsanchez@knox.edu>
2020-12-23 04:13:47 -0600

    add product model

---
+++ models/Products.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our Product model
+class Product extends Model {}
+
+// create fields/columns for Product model
+Product.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      price: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+        validate: {
+          isDecimal: true
+        }
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          isNumeric: true
+        }
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'category',
+          key: 'id'
+        }
+      }
+    },
+    {
+      sequelize,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'product'
+    }
+  );
+
+  module.exports = Product;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
 const Category = require('./Category');
+const Category = require('./Product');
 
-module.exports = { Category };
+module.exports = { Category, Product };
